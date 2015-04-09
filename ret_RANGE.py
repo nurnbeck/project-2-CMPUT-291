f87e11ad628b2c7929302dbc66736c25f5a69077
Yishuo Wang <yishuo@ualberta.ca>
2015-04-09 15:43:03 -0600

    Update ret_RANGE.py

--- ret_RANGE.py
+++ ret_RANGE.py
@@ -14,7 +14,7 @@ DB_FILE = "/tmp/yishuo_db/sample_db"
 SDB_FILE = "/tmp/yishuo_db/IndexFile"
 
 #DB_SIZE = 100000
-DB_SIZE = 10
+DB_SIZE = 200
 
 def ret_RANGE(filetype):
     if filetype == 'btree':
