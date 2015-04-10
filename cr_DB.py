bef4f1aa9addaff668da377634536162541b6413
Yishuo Wang <yishuo@ualberta.ca>
2015-04-10 15:28:19 -0600

    Update cr_DB.py

--- cr_DB.py
+++ cr_DB.py
@@ -18,8 +18,8 @@ DIR = "/tmp/yishuo_db/"
 DB_FILE = "/tmp/yishuo_db/sample_db"
 SDB_FILE = "/tmp/yishuo_db/IndexFile"
 
-#DB_SIZE = 100000
-DB_SIZE = 200
+DB_SIZE = 100000
+#DB_SIZE = 200
 SEED = 10000000
 
 def get_random():
